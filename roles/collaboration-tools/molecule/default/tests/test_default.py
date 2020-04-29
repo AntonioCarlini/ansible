8b74950298b6326ee350d7df2480014299797bfd
Antonio Carlini <antonio@acarlini.com>
2020-04-29 16:05:57 +0100

    Add molecule tests to collaboration-tools role.
    roles/collaboration-tools/.yamllint:
    roles/collaboration-tools/molecule/: molecule framework.
    
    roles/collaboration-tools/molecule/default/tests/test_default.py: Some simple tests to check that packages have been installed.
    
    roles/collaboration-tools/tasks/main.yml: Remove some comments.
    
    roles/collaboration-tools/meta/main.yml: Fix metadata to show tested platforms etc.
    
    collaboration-tools.yml: Remove trailing whitespace highlighted by ansible-lint.

---
+++ roles/collaboration-tools/molecule/default/tests/test_default.py
@@ -0,0 +1,15 @@
+"""Role testing files using testinfra."""
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+def test_collaboration_packages_installed(host):
+    assert host.package("skypeforlinux").is_installed
+    assert host.package("zoom").is_installed
+    assert host.package("slack-desktop").is_installed
