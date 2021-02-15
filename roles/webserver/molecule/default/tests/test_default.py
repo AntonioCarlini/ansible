5d70d30ae5d93c57e9bef39aff2942d5ad1a39a2
Antonio Carlini <antonio@acarlini.com>
2021-02-15 21:07:27 +0000

    feat: Fix testing of webserver role with docker
    CentOS installs httpd and php, so allow for those during the testing phase.

--- roles/webserver/molecule/default/tests/test_default.py
+++ roles/webserver/molecule/default/tests/test_default.py
@@ -1,15 +1,6 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
-
 def test_webserver_packages_installed(host):
-    assert host.package("apache2").is_installed
-    assert host.package("libapache2-mod-php").is_installed
-
+    assert (host.package("apache2").is_installed) or (host.package("httpd").is_installed)
+    assert (host.package("libapache2-mod-php").is_installed) or (host.package("php").is_installed)
